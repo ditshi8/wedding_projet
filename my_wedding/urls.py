a0b83491d75f9b83c533649ef7a07767165093a8
Dime Tshitemb <dimerciatshitemb2@gmail.com>
2023-07-27 09:59:32 +0200

    update crud

--- my_wedding/urls.py
+++ my_wedding/urls.py
@@ -4,6 +4,9 @@ from .views import venteView
 from .views import locationView
 from .views import meetingView
 from .views import contactView
+from .views import gestionContactView
+from .views import uptateContactView
+from .views import deleteContactView
 
 
 
@@ -12,5 +15,9 @@ urlpatterns = [
     path('vente/', venteView, name ="vente"),
     path('location/',locationView, name ="location"),
     path('meeting/', meetingView, name = "meeting"),
-    path('contact/', contactView, name = "contact")
+    path('contact/', contactView, name = "contact"),
+    path('gestion-contact/', gestionContactView, name = "gestion_contact"),
+    path('gestion-contact/update/<int:contact_id>/', uptateContactView, name = "update_contact"),
+    path('gestion-contact/delete/<int:contact_id>/', deleteContactView, name = "delete_contact")
+
 ]
--- my_wedding/views.py
+++ my_wedding/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 from .models import Contact
+
 
 # Create your views here.
 
@@ -36,7 +37,12 @@ def meetingView(request, template_name="wedding/pages/meeting.html"):
     return render(request,template_name)
 
 def contactView(request, template_name="wedding/pages/contact.html"):
-    print("je veux voir request", request)
+    context = {}
+    # SELECT * FROM Contact
+    contacts = Contact.objects.all()
+    # recuperer la liste de nos contacts
+    context["contacts"] = contacts
+
     # verification de la methode de la requele == POST
     if request.method == "POST":
         #Recuperation des valeur entrées par l'utilisateur
@@ -56,4 +62,60 @@ def contactView(request, template_name="wedding/pages/contact.html"):
         obj_contact.save()
 
 
-    return render(request, template_name)
+    return render(request, template_name, context)
+
+def gestionContactView(request, template_name = "wedding/pages/gestionContact.html"):
+    context = {}
+    # SELECT * FROM Contact
+    contacts = Contact.objects.all()
+    # recuperer la liste de nos contacts
+    context["contacts"] = contacts
+
+    if request.method == "POST":
+        # Recuperation des valeur entrées par l'utilisateur
+        name = request.POST['name']
+        email = request.POST['email']
+        content = request.POST['content']
+
+        # creation de notre objet Contact
+        obj_contact = Contact(
+            name=name,
+            email=email,
+            content=content
+        )
+
+        obj_contact.save()
+
+    return render(request, template_name, context)
+
+# Foction update
+
+def uptateContactView(request, contact_id ,template_name="wedding/pages/gestionContact.html"):
+    contact_one = Contact.objects.get(id=contact_id)
+    print("id", contact_one)
+    context = {}
+    contacts = Contact.objects.all()
+    context['contacts'] = contacts
+    context['contact_one'] = contact_one
+    #verifie si la condition == POST
+    if request.method == "POST":
+        # recuperation des donnees qui se trouve dans l'input et on le modifie
+        name = request.POST['name']
+        email = request.POST['email']
+        content = request.POST['content']
+        #assigner les valeur de l'input
+        contact_one.name = name
+        contact_one.email = email
+        contact_one.content = content
+        #sauvegarde de la modification
+        contact_one.save()
+
+    return  render(request,  template_name, context)
+
+def deleteContactView(request, contact_id):
+    contact_one = Contact.objects.get(id=contact_id)
+    contact_one.delete()
+
+    return redirect('gestion_contact')
+
+
